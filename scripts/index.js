102ce12127228aa66698274d232e87a1f56b4e01
Vikash Tiwari <95138490+vikashtiwari97@users.noreply.github.com>
2024-08-30 11:46:13 +0530

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -4,7 +4,7 @@ const giftbox = document.getElementById('merrywrap');
 const canvasC = document.getElementById('c');
 
 const config = {
-  birthdate: 'Aug 30, 2024',
+  birthdate: 'Aug 31, 2024',
   name: 'Khushi❤️'
 };
 
